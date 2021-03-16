87137a4439e08cb775db1e62ac422634c833a242
dmitryboz21 <dmitryboz21@yandex.com>
2021-03-16 21:42:35 +0300

    123

--- build/js/main.js
+++ build/js/main.js
@@ -27,4 +27,8 @@ $(document).ready(function () {
 $(document).ready(function () {
    // $("input[name='phone']").mask(" +7 (999) 999-99-99");
    autosize($('.textarea-autosize'));
+
+   $('.js-todo-list-item__chb').on('change', function () {
+
+   });
 });
--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
    // $("input[name='phone']").mask(" +7 (999) 999-99-99");
    autosize($('.textarea-autosize'));
-});
 
+   $('.js-todo-list-item__chb').on('change', function () {
+
+   });
+});
