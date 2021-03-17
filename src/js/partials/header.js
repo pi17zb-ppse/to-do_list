61a4cdf8fa6a5d65512274e49a207de704da7561
dmitryboz21 <dmitryboz21@yandex.com>
2021-03-17 06:45:33 +0300

    Закомментирован неактуальный код

--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function () {
 	$('.sh-burger').click(function () {
-		$(this).toggleClass('sh-burger--active');
+		//$(this).toggleClass('sh-burger--active');
 		$('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
 	});
 
