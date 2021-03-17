66dcb0390cdf114d1d9a45df14b0b1b651a92e8a
dmitryboz21 <dmitryboz21@yandex.com>
2021-03-17 06:45:53 +0300

    Закомментирован ещё неактуальный код

--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
 	$('.sh-burger').click(function () {
 		//$(this).toggleClass('sh-burger--active');
-		$('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
+		//$('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
 	});
 
 });
